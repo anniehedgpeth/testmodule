079abaeb9951bb4321785f596d7b7cf062e85c85
Annie Hedgpeth <annie@hashicorp.com>
2022-11-22 14:52:01 -0700

    test

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  backend "remote" {
-    hostname = "139.178.66.91"
-    organization = "hashi-annie"
+#   backend "remote" {
+#     hostname = "139.178.66.91"
+#     organization = "hashi-annie"
 
-    workspaces {
-      name = "testmodule"
-    }
-  }
+#     workspaces {
+#       name = "testmodule"
+#     }
+#   }
 }
